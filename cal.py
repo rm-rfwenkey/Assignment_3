c790ea1868748229fa8f12275ef9c08ed1792fc8
wenkey <muni.v.g@everest.engineering>
2021-08-10 22:56:33 +0530

    merged subtraction

--- cal.py
+++ cal.py
@@ -1,9 +1,12 @@
 def add(num1, num2):
     return num1 + num2
-
+# Function to subtract two numbers 
+def subtract(num1, num2):
+    return num1 - num2
 #select options
 print("Please select operation -\n" \
-        "1. Add\n")
+        "1. Add\n" \
+         "2. sub\n"  )
 
 
 # Take input from the user 
@@ -14,6 +17,9 @@ number_2 = int(input("Enter second number: "))
 if select == 1:
     print(number_1, "+", number_2, "=",
                     add(number_1, number_2))
+elif select == 2:
+    print(number_1, "-", number_2, "=",
+                    subtract(number_1, number_2))
 
 else:
     print("Invalid input")
